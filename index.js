98edef8ea26db45ca5ff80d5482c3a201e9ef4db
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-21 14:41:06 +0100

    Fixed issue with pagination

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Importing required modules
-const PORT = 5000;
 const express = require('express');
 const cheerio = require('cheerio'); // Parsing HTML
-const axios = require('axios');
+const axios = require('axios'); // Handles HTTP requests
 const j2c = require('json2csv').Parser; // Parse data to CSV format
 const { writeFileSync } = require('fs');
 
 const app = express();
+const PORT = 5000;
 
 // URLs for top anime and manga lists
 const topAnime = "https://myanimelist.net/topanime.php";
@@ -29,19 +28,17 @@ const getMedia = async (url, fileName1, fileName2, max) => {
                 const title = $(this).find("h3 a").text();
                 const score = $(this).find(".score .text").text();
 
-                data.push({
-                    rank, 
-                    title, 
-                    score 
-                });
+                data.push({ rank, title, score });
                 scrapedItems++;
             });
 
             // Move to the next page
-            const nextPage = $(".next").attr("href");
+            const nextPage = $(".pagination a.next").attr("href");
             if (!nextPage) break;
 
-            url = url + nextPage;
+            // url = url + nextPage;
+            // url = nextPage.startsWith("http") ? nextPage : url + nextPage;
+            url = new URL(nextPage, url).href;
         }
 
         // Convert data to CSV format and save it to a file
@@ -52,11 +49,14 @@ const getMedia = async (url, fileName1, fileName2, max) => {
         });
 
         // Convert data to JSON format and save it to a file
-        writeFileSync(fileName2, JSON.stringify(data.slice(0, max)), (err) => {
-            if (err) throw err;
+        writeFileSync(fileName2, JSON.stringify(data.slice(0, max), null, 2), (err) => {
+            if(err) throw err;
         });
     } catch (err) {
         console.log(err);
+    } finally {
+        server.close(() => { console.log('server closed'); });
+        // console.log("Scraping completed");
     }
 };
 
@@ -64,7 +64,8 @@ const getMedia = async (url, fileName1, fileName2, max) => {
 getMedia(topAnime, './Top100Anime.csv', './Top100Anime.json', 100);
 getMedia(topManga, './Top100Manga.csv', './Top100Manga.json', 100);
 
-app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
+const server = app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
+
 /*
 This code uses the `cheerio` library to parse HTML and the `axios` library to make HTTP requests. 
 It defines a function, `getMedia`, which scrapes media data from the provided URLs and saves it to CSV and JSON files. 
