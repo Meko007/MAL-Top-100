cdb19c8df3553d564ed4b8724c9d6d5fcb17482d
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-25 18:27:25 +0100

    destructuring

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const scraper = require('./scraper');
+const { getMedia } = require('./scraper');
 
 const app = express();
 const PORT = 5000;
 
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+});
+
 const topAnime = "https://myanimelist.net/topanime.php";
 const topManga = "https://myanimelist.net/topmanga.php";
 
-const scrapeAnime = scraper.getMedia(topAnime, './Top100Anime.csv', './Top100Anime.json', 100);
-const scrapeManga = scraper.getMedia(topManga, './Top100Manga.csv', './Top100Manga.json', 100);
-
-Promise.all([scrapeAnime, scrapeManga])
-    .then(() => {
-        console.log("Scraping completed");
-        app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-    })
-    .catch((error) => {
-        console.error("Error during scraping:", error);
-    });
+// getMedia(topAnime, './Top100Anime.csv', './Top100Anime.json', 100);
+// getMedia(topManga, './Top100Manga.csv', './Top100Manga.json', 100);
